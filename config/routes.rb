063a825231def06aba9e3cf2e6f38e5a1eb15613
Wessel Kuijpers <wrkuijpers@outlook.com>
2018-10-03 21:55:13 +0200

    added possiblility to look for users, look at their profile and ad them as friend to see what they are finished, made the last final tweaks and the app is now finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "user/registrations" }
-  root 'welcome#index'
+  root 'users#my_portfolio'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: "users#my_friends"
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  post 'add-friend', to: 'users#add_friend'
 
 end
